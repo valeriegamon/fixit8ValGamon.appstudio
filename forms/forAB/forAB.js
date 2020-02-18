25f809abc89814146a5c1a939d0caf36ad7be9fe
valeriegamon <valeriegamon@valeries-mbp.ddns.creighton.edu>
2020-02-18 12:53:43 -0600

    hehe

---
+++ forms/forAB/forAB.js
@@ -0,0 +1,27 @@
+/*
+You will be using anon-standard for loop to cycle through an array, outputing each item one per line, but backwards. That is, the last name in the array will be output first, etc.
+Hint: can you adjust something in the loop?
+Create a new form named ForAB.
+Create an array named 'names' that holds this data:
+
+Bob, Janet, Tom, Bob, Randy, Elizabeth, Kim, Nancy
+
+Each time the loop runs, it must output a name. So eventally your program will have output each name, one name per line.
+The output must be to the console, with the first line showing the last name in the array, then the second line showing the second to last name in the array, etc. until the last name output will be the one that was first in the array. Use this format:
+
+Nancy
+Kim
+Elizabeth
+Randy
+Bob
+Tom
+Janet
+Bob
+
+You may not reorder the array using a function or method.
+*/
+
+
+
+
+
