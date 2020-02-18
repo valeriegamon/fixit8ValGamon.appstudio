5793c0f4375bbba23f1679af2ba75ac5f0eb3eba
valeriegamon <valeriegamon@Valeries-MacBook-Pro.local>
2020-02-17 22:01:15 -0600

    hh

---
+++ forms/whileAB/whileAB.js
@@ -0,0 +1,12 @@
+let firstName = prompt("Enter your first name")
+let lastName = prompt("Enter your last name")
+let fullName = (firstName + lastName)
+let i=0
+
+while (i < fullName.length) {
+  console.log("Letter" + " " + (i+1) + " is" + " " + fullName.charAt(i))
+  i++
+  }
+  
+  
+  
