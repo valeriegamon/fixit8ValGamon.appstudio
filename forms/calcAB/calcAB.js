47f49a4312d506046246052d0ee9616dde6d4847
valeriegamon <valeriegamon@valeries-mbp.ddns.creighton.edu>
2020-02-18 13:50:46 -0600

    kjndkjsn

---
+++ forms/calcAB/calcAB.js
@@ -0,0 +1,19 @@
+/* 
+ABScenario: You will be writing a program that uses a function and a loop. 
+The function will take pairs of numbers from an array, add them, and give the sum back to the main program.
+
+Create a function named Calculate that takes 2 parameters and returns the product of the two numbers.
+In the main program, use a loop that calls the function as many times as needed.
+Each time the loop will call the function and pass 2 numbers from the array.
+Inside the loop, you will keep a running total of the sums of the pairs of numbers as they are returned by the function.
+When the loop is done, output the final sum to the console. 
+*/
+
+let myNumbers = [123, 433, 54326, 2234, 1919, 1123, 1928, 3374, 23, 190, 33874]
+console.log(myNumbers)
+
+
+
+
+
+
